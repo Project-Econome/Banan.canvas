34de83d06d900b321ce157a8b15e7456a5053a4e
Project Econome <108093514+Project-Econome@users.noreply.github.com>
2024-06-29 15:31:38 +0100

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,9 @@ import { ForgeExtension } from "@tryforge/forgescript";
 import { resolve } from "path";
 
 export class ForgeCanvas extends ForgeExtension {
-    name = "ForgeCanvas";
+    name = "BananCanvas";
     description = "Well the name says what it does.";
-    version = "1.0.0";
+    version = "2.0.0";
     
     init(): void {
         this.load(resolve(__dirname, "./functions"));

e5cbf9cb8fbad5e94da7f31ad3e910c1f1e52f6a
Project Econome <108093514+Project-Econome@users.noreply.github.com>
2024-06-29 15:49:32 +0100

    Update index.js

--- dist/index.js
+++ dist/index.js
@@ -1,12 +1,12 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.ForgeCanvas = void 0;
-const forgescript_1 = require("@tryforge/forgescript");
+const forgescript_1 = require("@econome/forgescript");
 const path_1 = require("path");
 class ForgeCanvas extends forgescript_1.ForgeExtension {
-    name = "ForgeCanvas";
+    name = "BananCanvas";
     description = "Well the name says what it does.";
-    version = "1.0.0";
+    version = "2.0.0";
     init() {
         this.load((0, path_1.resolve)(__dirname, "./functions"));
     }
